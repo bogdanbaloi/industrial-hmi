f1df02f48a014b4dbec870af3eda5a6e7cecdbee
Bogdan Baloi <baloibog@gmail.com>
2026-04-15 02:13:50 +0300

    Use Cairo renderer on Windows to prevent GL flicker

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "src/core/Application.h"
+#include <cstdlib>
 
 int main(int argc, char* argv[]) {
+#ifdef _WIN32
+    // Use Cairo renderer on Windows to avoid GL flicker
+    _putenv_s("GSK_RENDERER", "cairo");
+#endif
+
     auto& app = app::core::Application::instance();
 
     if (!app.initialize(argc, argv)) {
